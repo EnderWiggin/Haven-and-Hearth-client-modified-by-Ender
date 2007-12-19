f1b35dc9996e1a2c8a54132817f4c831d24abd02
Fredrik Tolf <fredrik@dolda2000.com>
2007-12-19 23:59:11 +0100

    Do error handling in applet.

--- src/haven/HavenApplet.java
+++ src/haven/HavenApplet.java
@@ -3,23 +3,32 @@ package haven;
 import java.applet.*;
 
 public class HavenApplet extends Applet {
-    Thread main;
+    ThreadGroup p;
+    HavenPanel h;
     
     public void destroy() {
-	main.interrupt();
+	p.interrupt();
 	if(Session.current != null)
 	    Session.current.close();
     }
     
+    public void start() {
+	p = new haven.error.ErrorHandler(new Runnable() {
+		public void run() {
+		    h = new HavenPanel(800, 600);
+		    add(h);
+		    h.init();
+		    Bootstrap b = new Bootstrap(h.ui);
+		    b.setaddr("www.seatribe.se");
+		    b.start();
+		    Thread main = new Thread(h, "Haven applet main thread");
+		    main.start();
+		}
+	    });
+    }
+
     public void init() {
 	resize(800, 600);
-	final HavenPanel h = new HavenPanel(800, 600);
-	add(h);
-	h.init();
-	Bootstrap b = new Bootstrap(h.ui);
-	b.setaddr("www.seatribe.se");
-	b.start();
-	main = new Thread(h, "Haven applet main thread");
-	main.start();
+	start();
     }
 }
