9afb26739ebb8a9c1897fb8f0ee3597864918462
Fredrik Tolf <fredrik@dolda2000.com>
2008-01-01 16:16:51 +0100

    Made a real ErrorStatus UI for the applet.

--- src/haven/HavenApplet.java
+++ src/haven/HavenApplet.java
@@ -1,10 +1,58 @@
 package haven;
 
 import java.applet.*;
+import java.awt.*;
 
 public class HavenApplet extends Applet {
     ThreadGroup p;
     HavenPanel h;
+    
+    private class ErrorPanel extends Canvas implements haven.error.ErrorStatus {
+	String status = "";
+	
+	public ErrorPanel() {
+	    setBackground(Color.BLACK);
+	}
+	
+	public void goterror(Throwable t) {
+	    p.interrupt();
+	    HavenApplet.this.remove(h);
+	    setSize(HavenApplet.this.getSize());
+	    HavenApplet.this.add(this);
+	    repaint();
+	}
+	
+	public void connecting() {
+	    status = "Connecting to error report server...";
+	    repaint();
+	}
+	
+	public void sending() {
+	    status = "Sending error report...";
+	    repaint();
+	}
+	
+	public void done() {
+	    status = "Done";
+	    repaint();
+	}
+	
+	public void senderror(Exception e) {
+	    status = "Could not send error report";
+	    repaint();
+	}
+	
+	public void paint(Graphics g) {
+	    g.setColor(Color.BLACK);
+	    g.fillRect(0, 0, getWidth(), getHeight());
+	    g.setColor(Color.WHITE);
+	    FontMetrics m = g.getFontMetrics();
+	    int y = 0;
+	    g.drawString("An error has occurred.", 0, y + m.getAscent());
+	    y += m.getHeight();
+	    g.drawString(status, 0, y + m.getAscent());
+	}
+    }
     
     public void destroy() {
 	p.interrupt();
@@ -25,6 +73,6 @@ public class HavenApplet extends Applet {
 		    Thread main = new Thread(Utils.tg(), h, "Haven applet main thread");
 		    main.start();
 		}
-	    });
+	    }, new ErrorPanel());
     }
 }
