2f304ccc8b6f9ae1b958d88128f07c99b15c68b2
Fredrik Tolf <fredrik@dolda2000.com>
2008-05-05 19:42:22 +0200

    Fixed IBox drawing error.

--- src/haven/IBox.java
+++ src/haven/IBox.java
@@ -44,9 +44,9 @@ public class IBox {
 	
 	public void draw(GOut g, Coord tl, Coord sz) {
 		g.image(bt, tl.add(new Coord(ctl.sz().x, 0)), new Coord(sz.x - ctr.sz().x - ctl.sz().x, bt.sz().y));
-		g.image(bb, tl.add(new Coord(cbl.sz().x, sz.y - bb.sz().y + tl.y)), new Coord(sz.x - cbr.sz().x - cbl.sz().x, bb.sz().y));
+		g.image(bb, tl.add(new Coord(cbl.sz().x, sz.y - bb.sz().y)), new Coord(sz.x - cbr.sz().x - cbl.sz().x, bb.sz().y));
 		g.image(bl, tl.add(new Coord(0, ctl.sz().y)), new Coord(bl.sz().x, sz.y - cbl.sz().y - ctl.sz().y));
-		g.image(br, tl.add(new Coord(sz.x - br.sz().x + tl.x, ctr.sz().y)), new Coord(br.sz().x, sz.y - cbr.sz().y - ctr.sz().y));
+		g.image(br, tl.add(new Coord(sz.x - br.sz().x, ctr.sz().y)), new Coord(br.sz().x, sz.y - cbr.sz().y - ctr.sz().y));
 		g.image(ctl, tl);
 		g.image(ctr, tl.add(sz.x - ctr.sz().x, 0));
 		g.image(cbl, tl.add(0, sz.y - cbl.sz().y));
