4a50cfeb59f4f8e3d7d4317a3c961a2cc3d05481
Fredrik Tolf <fredrik@dolda2000.com>
2008-08-30 02:52:18 +0200

    Bugfixed the avaview rendering the slightest bit.

--- src/haven/Avaview.java
+++ src/haven/Avaview.java
@@ -3,10 +3,12 @@ package haven;
 import java.awt.Color;
 
 public class Avaview extends Widget {
-	public static final Coord dasz = new Coord(70, 70);
+	public static final Coord dasz = new Coord(74, 74);
 	private Coord asz;
 	int avagob;
 	public Color color = Color.WHITE;
+        public static final Coord unborder = new Coord(2, 2);
+        public static final Tex missing = Resource.loadtex("gfx/hud/equip/missing");
 	
 	static {
 		Widget.addtype("av", new WidgetFactory() {
@@ -17,7 +19,7 @@ public class Avaview extends Widget {
 	}
 	
 	public Avaview(Coord c, Widget parent, int avagob, Coord asz) {
-		super(c, asz.add(Window.wbox.bisz()), parent);
+		super(c, asz.add(Window.wbox.bisz()).add(unborder.mul(2).inv()), parent);
 		this.avagob = avagob;
 		this.asz = asz;
 	}
@@ -27,21 +29,25 @@ public class Avaview extends Widget {
 	}
 	
 	public void draw(GOut g) {
-		g.chcolor(color);
-		Window.wbox.draw(g, Coord.z, asz.add(Window.wbox.bisz()));
-		g.chcolor(Color.WHITE);
 		Gob gob = ui.sess.glob.oc.getgob(avagob);
-		if(gob == null)
-			return;
-		Avatar ava = gob.getattr(Avatar.class);
-		if(ava == null)
-			return;
-		GOut g2 = g.reclip(Window.wbox.tloff(), asz);
-		g2.image(Equipory.bg, new Coord(Equipory.bg.sz().x / 2 - asz.x / 2, 20).inv());
-		Tex at = ava.tex();
-		Coord tsz = new Coord((at.sz().x * asz.x) / dasz.x, (at.sz().y * asz.y) / dasz.y);
-		int yo = (20 * asz.y) / dasz.y;
-		g2.image(at, new Coord(tsz.x / 2 - asz.x / 2, yo).inv(), tsz);
+                Avatar ava = null;
+		if(gob != null)
+        		ava = gob.getattr(Avatar.class);
+                GOut g2 = g.reclip(Window.wbox.tloff().add(unborder.inv()), asz);
+                Tex at;
+                int yo;
+		if(ava == null) {
+                        at = missing;
+                        yo = 0;
+                } else {
+                        g2.image(Equipory.bg, new Coord(Equipory.bg.sz().x / 2 - asz.x / 2, 20).inv());
+                        at = ava.tex();
+                        yo = (20 * asz.y) / dasz.y;
+                }
+                Coord tsz = new Coord((at.sz().x * asz.x) / dasz.x, (at.sz().y * asz.y) / dasz.y);
+                g2.image(at, new Coord(tsz.x / 2 - asz.x / 2, yo).inv(), tsz);
+		g.chcolor(color);
+		Window.wbox.draw(g, Coord.z, asz.add(Window.wbox.bisz()).add(unborder.mul(2).inv()));
 	}
 	
 	public boolean mousedown(Coord c, int button) {
