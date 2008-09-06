84c0de28b1a1cad43e16fdc577b9668bad58d92a
Fredrik Tolf <fredrik@dolda2000.com>
2008-09-06 19:59:49 +0200

    Format individual reports as well.

--- src/haven/error/HtmlReporter.java
+++ src/haven/error/HtmlReporter.java
@@ -2,11 +2,11 @@ package haven.error;
 
 import java.io.*;
 import java.util.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.text.*;
 
 public class HtmlReporter {
-    public static final DateFormat dfmt = new SimpleDateFormat();
+    public static final DateFormat dfmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final NumberFormat ifmt = NumberFormat.getInstance();
     
     public static String htmlhead(String title) {
 	StringBuilder buf = new StringBuilder();
@@ -42,6 +42,61 @@ public class HtmlReporter {
 		buf.append(c);
 	}
 	return(buf.toString());
+    }
+
+    public static String htmlbt(StackTraceElement[] bt) {
+	StringBuilder buf = new StringBuilder();
+	buf.append("<table class=\"bt\">\n");
+	buf.append("<tr><th>Class</th><th>Function</th><th>File</th><th>Line</th></tr>\n");
+	for(StackTraceElement e : bt) {
+	    if(e.isNativeMethod())
+		buf.append("<tr class=\"native\">");
+	    else
+		buf.append("<tr>");
+	    buf.append("<td>" + htmlq(e.getClassName()) + "</td>");
+	    buf.append("<td>" + htmlq(e.getMethodName()) + "</td>");
+	    buf.append("<td>" + htmlq(e.getFileName()) + "</td>");
+	    buf.append("<td>" + htmlq(Integer.toString(e.getLineNumber())) + "</td>");
+	    buf.append("</tr>\n");
+	}
+	buf.append("<table>\n");
+	return(buf.toString());
+    }
+
+    public static void makereport(OutputStream outs, Report rep) throws IOException {
+	PrintWriter out = new PrintWriter(new OutputStreamWriter(outs, "UTF-8"));
+	out.print(htmlhead("Error Report"));
+	out.println("<h1>Error Report</h1>");
+	
+	out.println("<p>Reported at: " + htmlq(dfmt.format(new Date(rep.time))) + "</p>");
+	out.println("<h2>Properties</h2>");
+	out.println("<table>");
+	out.println("<tr><th>Name</th><th>Value</th>");
+	for(String key : rep.props.keySet()) {
+	    Object val = rep.props.get(key);
+	    String vals;
+	    if(val instanceof Number) {
+		vals = ifmt.format((Number)val);
+	    } else if(val instanceof Date) {
+		vals = dfmt.format((Number)val);
+	    } else {
+		vals = val.toString();
+	    }
+	    out.print("    <tr>");
+	    out.print("<td>" + htmlq(key) + "</td><td>" + htmlq(vals) + "</td>");
+	    out.println("    </tr>");
+	}
+	out.println("</table>");
+	
+	out.println("<h2>Exception chain</h2>");
+	for(Throwable t = rep.t; t != null; t = t.getCause()) {
+	    out.println("<h3>" + htmlq(t.getClass().getName()) + "</h3>");
+	    out.println("<p>" + htmlq(t.getMessage()) + "</p>");
+	    out.print(htmlbt(t.getStackTrace()));
+	}
+	
+	out.print(htmltail());
+	out.flush();
     }
 
     public static void makeindex(OutputStream outs, Map<File, Report> reports, Map<File, Exception> failed) throws IOException {
@@ -117,11 +172,21 @@ public class HtmlReporter {
 	    }
 	}
 	
-	OutputStream out = new FileOutputStream(new File(outdir, "index.html"));
+	OutputStream out;
+	out = new FileOutputStream(new File(outdir, "index.html"));
 	try {
 	    makeindex(out, reports, failed);
 	} finally {
 	    out.close();
+	}
+	
+	for(File f : reports.keySet()) {
+	    out = new FileOutputStream(new File(outdir, f.getName() + ".html"));
+	    try {
+		makereport(out, reports.get(f));
+	    } finally {
+		out.close();
+	    }
 	}
     }
 }
