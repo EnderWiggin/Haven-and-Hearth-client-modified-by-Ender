bbab9e4d4ccc38d6e87f449b40c77c6db3df8823
Fredrik Tolf <fredrik@dolda2000.com>
2008-09-09 16:24:36 +0200

    Switched Utils to reasonable whitespace usage.

--- src/haven/Utils.java
+++ src/haven/Utils.java
@@ -11,297 +11,297 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class Utils {
-	private static Preferences prefs = null;
-	public static java.awt.image.ColorModel rgbm = java.awt.image.ColorModel.getRGBdefault();
-	private static Background bgworker = null;
+    private static Preferences prefs = null;
+    public static java.awt.image.ColorModel rgbm = java.awt.image.ColorModel.getRGBdefault();
+    private static Background bgworker = null;
 
-	static Coord imgsz(BufferedImage img) {
-		return(new Coord(img.getWidth(), img.getHeight()));
-	}
+    static Coord imgsz(BufferedImage img) {
+	return(new Coord(img.getWidth(), img.getHeight()));
+    }
 	
-	public static class Background extends Thread {
-		Queue<Runnable> q = new LinkedList<Runnable>();
+    public static class Background extends Thread {
+	Queue<Runnable> q = new LinkedList<Runnable>();
 		
-		public Background() {
-			super(tg(), "Haven deferred procedure thread");
-			setDaemon(true);
-			start();
-		}
+	public Background() {
+	    super(tg(), "Haven deferred procedure thread");
+	    setDaemon(true);
+	    start();
+	}
 		
-		public void run() {
-			try {
-				while(true) {
-					Runnable cur;
-					synchronized(q) {
-						while((cur = q.poll()) == null)
-							q.wait();
-					}
-					cur.run();
-					cur = null;
-				}
-			} catch(InterruptedException e) {}
-		}
-		
-		public void defer(Runnable r) {
-			synchronized(q) {
-				q.add(r);
-				q.notify();
-			}
-		}
-	}
-	
-	public static void defer(Runnable r) {
-		synchronized(Utils.class) {
-			if(bgworker == null)
-				bgworker = new Background();
-		}
-		bgworker.defer(r);
-	}
-	
-	static void drawgay(BufferedImage t, BufferedImage img, Coord c) {
-		Coord sz = imgsz(img);
-		for(int y = 0; y < sz.y; y++) {
-			for(int x = 0; x < sz.x; x++) {
-				int p = img.getRGB(x, y);
-				if(Utils.rgbm.getAlpha(p) > 128) {
-					if((p & 0x00ffffff) == 0x00ff0080)
-						t.setRGB(x + c.x, y + c.y, 0);
-					else
-						t.setRGB(x + c.x, y + c.y, p);
-				}
-			}
-		}
-	}
-	
-	public static int drawtext(Graphics g, String text, Coord c) {
-		java.awt.FontMetrics m = g.getFontMetrics();
-		g.drawString(text, c.x, c.y + m.getAscent());
-		return(m.getHeight());
-	}
-	
-	static Coord textsz(Graphics g, String text) {
-		java.awt.FontMetrics m = g.getFontMetrics();
-		java.awt.geom.Rectangle2D ts = m.getStringBounds(text, g);
-		return(new Coord((int)ts.getWidth(), (int)ts.getHeight()));
-	}
-	
-	static void aligntext(Graphics g, String text, Coord c, double ax, double ay) {
-		java.awt.FontMetrics m = g.getFontMetrics();
-		java.awt.geom.Rectangle2D ts = m.getStringBounds(text, g);
-		g.drawString(text, (int)(c.x - ts.getWidth() * ax), (int)(c.y + m.getAscent() - ts.getHeight() * ay));
-	}
-	
-	static ThreadGroup tg() {
-		return(Thread.currentThread().getThreadGroup());
-	}
-
-	static void line(Graphics g, Coord c1, Coord c2) {
-		g.drawLine(c1.x, c1.y, c2.x, c2.y);
-	}
-	
-	static void AA(Graphics g) {
-		java.awt.Graphics2D g2 = (java.awt.Graphics2D)g;
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);		
-	}
-	
-	static synchronized String getpref(String prefname, String def) {
-		try {
-			if(prefs == null)
-				prefs = Preferences.userNodeForPackage(Utils.class);
-			return(prefs.get(prefname, def));
-		} catch(SecurityException e) {
-			return(def);
-		}
-	}
-	
-	static synchronized void setpref(String prefname, String val) {
-		try {
-			if(prefs == null)
-				prefs = Preferences.userNodeForPackage(Utils.class);
-			prefs.put(prefname, val);
-		} catch(SecurityException e) {
-		}
-	}
-	
-	static int ub(byte b) {
-		if(b < 0)
-			return(256 + b);
-		else
-			return(b);
-	}
-	
-	static byte sb(int b) {
-		if(b > 127)
-			return((byte)(-256 + b));
-		else
-			return((byte)b);
-	}
-	
-	static int uint16d(byte[] buf, int off) {
-		return(ub(buf[off]) + (ub(buf[off + 1]) * 256));
-	}
-	
-	static int int16d(byte[] buf, int off) {
-		int u = uint16d(buf, off);
-		if(u > 32767)
-			return(-65536 + u);
-		else
-			return(u);
-	}
-	
-	static long uint32d(byte[] buf, int off) {
-		return(ub(buf[off]) + (ub(buf[off + 1]) * 256) + (ub(buf[off + 2]) * 65536) + (ub(buf[off + 3]) * 16777216));
-	}
-	
-	static void uint32e(long num, byte[] buf, int off) {
-		buf[off] = sb((int)(num & 0xff));
-		buf[off + 1] = sb((int)((num & 0xff00) >> 8));
-		buf[off + 2] = sb((int)((num & 0xff0000) >> 16));
-		buf[off + 3] = sb((int)((num & 0xff000000) >> 24));
-	}
-	
-	static int int32d(byte[] buf, int off) {
-		long u = uint32d(buf, off);
-		if(u > Integer.MAX_VALUE)
-			return((int)((((long)Integer.MIN_VALUE) * 2) - u));
-		else
-			return((int)u);
-	}
-	
-	static void int32e(int num, byte[] buf, int off) {
-		if(num < 0)
-			uint32e(0x100000000L + ((long)num), buf, off);
-		else
-			uint32e(num, buf, off);
-	}
-	
-	static void uint16e(int num, byte[] buf, int off) {
-		buf[off] = sb(num & 0xff);
-		buf[off + 1] = sb((num & 0xff00) >> 8);
-	}
-	
-	static String strd(byte[] buf, int[] off) {
-		int i;
-		for(i = off[0]; buf[i] != 0; i++);
-		String ret;
-		try {
-			ret = new String(buf, off[0], i - off[0], "utf-8");
-		} catch(UnsupportedEncodingException e) {
-			throw(new RuntimeException(e));
-		}
-		off[0] = i + 1;
-		return(ret);
-	}
-	
-	static char num2hex(int num) {
-		if(num < 10)
-			return((char)('0' + num));
-		else
-			return((char)('A' + num - 10));
-	}
-	
-	static int hex2num(char hex) {
-		if((hex >= '0') && (hex <= '9'))
-			return(hex - '0');
-		else if((hex >= 'a') && (hex <= 'f'))
-			return(hex - 'a' + 10);
-		else if((hex >= 'A') && (hex <= 'F'))
-			return(hex - 'A' + 10);
-		else
-			throw(new RuntimeException());
-	}
-
-	static String byte2hex(byte[] in) {
-		StringBuilder buf = new StringBuilder();
-		for(byte b : in) {
-			buf.append(num2hex((b & 0xf0) >> 4));
-			buf.append(num2hex(b & 0x0f));
-		}
-		return(buf.toString());
-	}
-
-	static byte[] hex2byte(String hex) {
-		if(hex.length() % 2 != 0)
-			throw(new RuntimeException("Invalid hex-encoded string"));
-		byte[] ret = new byte[hex.length() / 2];
-		for(int i = 0, o = 0; i < hex.length(); i += 2, o++)
-			ret[o] = (byte)((hex2num(hex.charAt(i)) << 4) | hex2num(hex.charAt(i + 1)));
-		return(ret);
-	}
-	
-	static String[] splitwords(String text) {
-		ArrayList<String> words = new ArrayList<String>();
-		StringBuilder buf = new StringBuilder();
-		String st = "ws";
-		int i = 0;
-		while(i < text.length()) {
-			char c = text.charAt(i);
-			if(st == "ws") {
-				if(!Character.isWhitespace(c))
-					st = "word";
-				else
-					i++;
-			} else if(st == "word") {
-				if(c == '"') {
-					st = "quote";
-					i++;
-				} else if(c == '\\') {
-					st = "squote";
-					i++;
-				} else if(Character.isWhitespace(c)) {
-					words.add(buf.toString());
-					buf = new StringBuilder();
-					st = "ws";
-				} else {
-					buf.append(c);
-					i++;
-				}
-			} else if(st == "quote") {
-				if(c == '"') {
-					st = "word";
-					i++;
-				} else if(c == '\\') {
-					st = "sqquote";
-					i++;
-				} else {
-					buf.append(c);
-					i++;
-				}
-			} else if(st == "squote") {
-				buf.append(c);
-				i++;
-				st = "word";
-			} else if(st == "sqquote") {
-				buf.append(c);
-				i++;
-				st = "quote";
-			}
-		}
-		if(st == "word")
-			words.add(buf.toString());
-		if((st != "ws") && (st != "word"))
-			return(null);
-		return(words.toArray(new String[0]));
-	}
-	
-	static int atoi(String a) {
-		try {
-			return(Integer.parseInt(a));
-		} catch(NumberFormatException e) {
-			return(0);
-		}
-	}
-    
-	static byte[] readall(InputStream in) throws IOException {
-		byte[] buf = new byte[4096];
-		int off = 0;
+	public void run() {
+	    try {
 		while(true) {
-			if(off == buf.length)
-				buf = Arrays.copyOf(buf, buf.length * 2);
-			int ret = in.read(buf, off, buf.length - off);
-			if(ret < 0)
-				return(Arrays.copyOf(buf, off));
-			off += ret;
+		    Runnable cur;
+		    synchronized(q) {
+			while((cur = q.poll()) == null)
+			    q.wait();
+		    }
+		    cur.run();
+		    cur = null;
 		}
+	    } catch(InterruptedException e) {}
 	}
+		
+	public void defer(Runnable r) {
+	    synchronized(q) {
+		q.add(r);
+		q.notify();
+	    }
+	}
+    }
+	
+    public static void defer(Runnable r) {
+	synchronized(Utils.class) {
+	    if(bgworker == null)
+		bgworker = new Background();
+	}
+	bgworker.defer(r);
+    }
+	
+    static void drawgay(BufferedImage t, BufferedImage img, Coord c) {
+	Coord sz = imgsz(img);
+	for(int y = 0; y < sz.y; y++) {
+	    for(int x = 0; x < sz.x; x++) {
+		int p = img.getRGB(x, y);
+		if(Utils.rgbm.getAlpha(p) > 128) {
+		    if((p & 0x00ffffff) == 0x00ff0080)
+			t.setRGB(x + c.x, y + c.y, 0);
+		    else
+			t.setRGB(x + c.x, y + c.y, p);
+		}
+	    }
+	}
+    }
+	
+    public static int drawtext(Graphics g, String text, Coord c) {
+	java.awt.FontMetrics m = g.getFontMetrics();
+	g.drawString(text, c.x, c.y + m.getAscent());
+	return(m.getHeight());
+    }
+	
+    static Coord textsz(Graphics g, String text) {
+	java.awt.FontMetrics m = g.getFontMetrics();
+	java.awt.geom.Rectangle2D ts = m.getStringBounds(text, g);
+	return(new Coord((int)ts.getWidth(), (int)ts.getHeight()));
+    }
+	
+    static void aligntext(Graphics g, String text, Coord c, double ax, double ay) {
+	java.awt.FontMetrics m = g.getFontMetrics();
+	java.awt.geom.Rectangle2D ts = m.getStringBounds(text, g);
+	g.drawString(text, (int)(c.x - ts.getWidth() * ax), (int)(c.y + m.getAscent() - ts.getHeight() * ay));
+    }
+	
+    static ThreadGroup tg() {
+	return(Thread.currentThread().getThreadGroup());
+    }
+
+    static void line(Graphics g, Coord c1, Coord c2) {
+	g.drawLine(c1.x, c1.y, c2.x, c2.y);
+    }
+	
+    static void AA(Graphics g) {
+	java.awt.Graphics2D g2 = (java.awt.Graphics2D)g;
+	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);		
+    }
+	
+    static synchronized String getpref(String prefname, String def) {
+	try {
+	    if(prefs == null)
+		prefs = Preferences.userNodeForPackage(Utils.class);
+	    return(prefs.get(prefname, def));
+	} catch(SecurityException e) {
+	    return(def);
+	}
+    }
+	
+    static synchronized void setpref(String prefname, String val) {
+	try {
+	    if(prefs == null)
+		prefs = Preferences.userNodeForPackage(Utils.class);
+	    prefs.put(prefname, val);
+	} catch(SecurityException e) {
+	}
+    }
+	
+    static int ub(byte b) {
+	if(b < 0)
+	    return(256 + b);
+	else
+	    return(b);
+    }
+	
+    static byte sb(int b) {
+	if(b > 127)
+	    return((byte)(-256 + b));
+	else
+	    return((byte)b);
+    }
+	
+    static int uint16d(byte[] buf, int off) {
+	return(ub(buf[off]) + (ub(buf[off + 1]) * 256));
+    }
+	
+    static int int16d(byte[] buf, int off) {
+	int u = uint16d(buf, off);
+	if(u > 32767)
+	    return(-65536 + u);
+	else
+	    return(u);
+    }
+	
+    static long uint32d(byte[] buf, int off) {
+	return(ub(buf[off]) + (ub(buf[off + 1]) * 256) + (ub(buf[off + 2]) * 65536) + (ub(buf[off + 3]) * 16777216));
+    }
+	
+    static void uint32e(long num, byte[] buf, int off) {
+	buf[off] = sb((int)(num & 0xff));
+	buf[off + 1] = sb((int)((num & 0xff00) >> 8));
+	buf[off + 2] = sb((int)((num & 0xff0000) >> 16));
+	buf[off + 3] = sb((int)((num & 0xff000000) >> 24));
+    }
+	
+    static int int32d(byte[] buf, int off) {
+	long u = uint32d(buf, off);
+	if(u > Integer.MAX_VALUE)
+	    return((int)((((long)Integer.MIN_VALUE) * 2) - u));
+	else
+	    return((int)u);
+    }
+	
+    static void int32e(int num, byte[] buf, int off) {
+	if(num < 0)
+	    uint32e(0x100000000L + ((long)num), buf, off);
+	else
+	    uint32e(num, buf, off);
+    }
+	
+    static void uint16e(int num, byte[] buf, int off) {
+	buf[off] = sb(num & 0xff);
+	buf[off + 1] = sb((num & 0xff00) >> 8);
+    }
+	
+    static String strd(byte[] buf, int[] off) {
+	int i;
+	for(i = off[0]; buf[i] != 0; i++);
+	String ret;
+	try {
+	    ret = new String(buf, off[0], i - off[0], "utf-8");
+	} catch(UnsupportedEncodingException e) {
+	    throw(new RuntimeException(e));
+	}
+	off[0] = i + 1;
+	return(ret);
+    }
+	
+    static char num2hex(int num) {
+	if(num < 10)
+	    return((char)('0' + num));
+	else
+	    return((char)('A' + num - 10));
+    }
+	
+    static int hex2num(char hex) {
+	if((hex >= '0') && (hex <= '9'))
+	    return(hex - '0');
+	else if((hex >= 'a') && (hex <= 'f'))
+	    return(hex - 'a' + 10);
+	else if((hex >= 'A') && (hex <= 'F'))
+	    return(hex - 'A' + 10);
+	else
+	    throw(new RuntimeException());
+    }
+
+    static String byte2hex(byte[] in) {
+	StringBuilder buf = new StringBuilder();
+	for(byte b : in) {
+	    buf.append(num2hex((b & 0xf0) >> 4));
+	    buf.append(num2hex(b & 0x0f));
+	}
+	return(buf.toString());
+    }
+
+    static byte[] hex2byte(String hex) {
+	if(hex.length() % 2 != 0)
+	    throw(new RuntimeException("Invalid hex-encoded string"));
+	byte[] ret = new byte[hex.length() / 2];
+	for(int i = 0, o = 0; i < hex.length(); i += 2, o++)
+	    ret[o] = (byte)((hex2num(hex.charAt(i)) << 4) | hex2num(hex.charAt(i + 1)));
+	return(ret);
+    }
+	
+    static String[] splitwords(String text) {
+	ArrayList<String> words = new ArrayList<String>();
+	StringBuilder buf = new StringBuilder();
+	String st = "ws";
+	int i = 0;
+	while(i < text.length()) {
+	    char c = text.charAt(i);
+	    if(st == "ws") {
+		if(!Character.isWhitespace(c))
+		    st = "word";
+		else
+		    i++;
+	    } else if(st == "word") {
+		if(c == '"') {
+		    st = "quote";
+		    i++;
+		} else if(c == '\\') {
+		    st = "squote";
+		    i++;
+		} else if(Character.isWhitespace(c)) {
+		    words.add(buf.toString());
+		    buf = new StringBuilder();
+		    st = "ws";
+		} else {
+		    buf.append(c);
+		    i++;
+		}
+	    } else if(st == "quote") {
+		if(c == '"') {
+		    st = "word";
+		    i++;
+		} else if(c == '\\') {
+		    st = "sqquote";
+		    i++;
+		} else {
+		    buf.append(c);
+		    i++;
+		}
+	    } else if(st == "squote") {
+		buf.append(c);
+		i++;
+		st = "word";
+	    } else if(st == "sqquote") {
+		buf.append(c);
+		i++;
+		st = "quote";
+	    }
+	}
+	if(st == "word")
+	    words.add(buf.toString());
+	if((st != "ws") && (st != "word"))
+	    return(null);
+	return(words.toArray(new String[0]));
+    }
+	
+    static int atoi(String a) {
+	try {
+	    return(Integer.parseInt(a));
+	} catch(NumberFormatException e) {
+	    return(0);
+	}
+    }
+    
+    static byte[] readall(InputStream in) throws IOException {
+	byte[] buf = new byte[4096];
+	int off = 0;
+	while(true) {
+	    if(off == buf.length)
+		buf = Arrays.copyOf(buf, buf.length * 2);
+	    int ret = in.read(buf, off, buf.length - off);
+	    if(ret < 0)
+		return(Arrays.copyOf(buf, off));
+	    off += ret;
+	}
+    }
 }
