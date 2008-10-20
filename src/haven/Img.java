7641cb16b51d329f5c97162b29c16f3c1c70057e
Fredrik Tolf <fredrik@dolda2000.com>
2008-10-20 02:20:29 +0200

    Switched Img to reasonable whitespace usage.

--- src/haven/Img.java
+++ src/haven/Img.java
@@ -1,30 +1,30 @@
 package haven;
 
 public class Img extends Widget {
-	private Tex img;
+    private Tex img;
 	
-	static {
-		Widget.addtype("img", new WidgetFactory() {
-			public Widget create(Coord c, Widget parent, Object[] args) {
-				return(new Img(c, Resource.loadtex((String)args[0]), parent));
-			}
-		});
-	}
-	
-	public void draw(GOut g) {
-		synchronized(img) {
-			g.image(img, Coord.z);
+    static {
+	Widget.addtype("img", new WidgetFactory() {
+		public Widget create(Coord c, Widget parent, Object[] args) {
+		    return(new Img(c, Resource.loadtex((String)args[0]), parent));
 		}
-	}
+	    });
+    }
 	
-	public Img(Coord c, Tex img, Widget parent) {
-		super(c, img.sz(), parent);
-		this.img = img;
+    public void draw(GOut g) {
+	synchronized(img) {
+	    g.image(img, Coord.z);
 	}
+    }
 	
-	public void uimsg(String name, Object... args) {
-		if(name == "ch") {
-			img = Resource.loadtex((String)args[0]);
-		}
+    public Img(Coord c, Tex img, Widget parent) {
+	super(c, img.sz(), parent);
+	this.img = img;
+    }
+	
+    public void uimsg(String name, Object... args) {
+	if(name == "ch") {
+	    img = Resource.loadtex((String)args[0]);
 	}
+    }
 }
