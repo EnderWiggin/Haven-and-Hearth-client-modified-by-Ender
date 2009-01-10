1ebf45835a85b5d6a811971523a6903242ea4d47
Fredrik Tolf <fredrik@dolda2000.com>
2009-01-10 02:17:34 +0100

    Switched TexRT and ILM to reasonable whitespace usage.

--- src/haven/ILM.java
+++ src/haven/ILM.java
@@ -13,66 +13,66 @@ import java.util.Collection;
 import javax.media.opengl.GL;
 
 public class ILM extends TexRT {
-	public final static BufferedImage ljusboll;
-	Collection<Lumin> ll;
-	TexI lbtex;
-	Color amb;
+    public final static BufferedImage ljusboll;
+    Collection<Lumin> ll;
+    TexI lbtex;
+    Color amb;
 	
-	static {
-		int sz = 200, min = 50;
-		BufferedImage lb = new BufferedImage(sz, sz, BufferedImage.TYPE_INT_ARGB);
-		Graphics g = lb.createGraphics();
-		for(int y = 0; y < sz; y++) {
-			for(int x = 0; x < sz; x++) {
-				double dx = sz / 2 - x;
-				double dy = sz / 2 - y;
-				double d = Math.sqrt(dx * dx + dy * dy);
-				int gs;
-				if(d > sz / 2)
-					gs = 255;
-				else if(d < min)
-					gs = 0;
-				else
-					gs = (int)(((d - min) / ((sz / 2) - min)) * 255);
-				gs /= 2;
-				Color c = new Color(gs, gs, gs, 128 - gs);
-				g.setColor(c);
-				g.fillRect(x, y, 1, 1);
-			}
-		}
-		ljusboll = lb;
+    static {
+	int sz = 200, min = 50;
+	BufferedImage lb = new BufferedImage(sz, sz, BufferedImage.TYPE_INT_ARGB);
+	Graphics g = lb.createGraphics();
+	for(int y = 0; y < sz; y++) {
+	    for(int x = 0; x < sz; x++) {
+		double dx = sz / 2 - x;
+		double dy = sz / 2 - y;
+		double d = Math.sqrt(dx * dx + dy * dy);
+		int gs;
+		if(d > sz / 2)
+		    gs = 255;
+		else if(d < min)
+		    gs = 0;
+		else
+		    gs = (int)(((d - min) / ((sz / 2) - min)) * 255);
+		gs /= 2;
+		Color c = new Color(gs, gs, gs, 128 - gs);
+		g.setColor(c);
+		g.fillRect(x, y, 1, 1);
+	    }
 	}
+	ljusboll = lb;
+    }
 	
-	public ILM(Coord sz) {
-		super(sz);
-		amb = new Color(0, 0, 0, 0);
-	}
+    public ILM(Coord sz) {
+	super(sz);
+	amb = new Color(0, 0, 0, 0);
+    }
 	
-	protected Color setenv(GL gl) {
-		gl.glTexEnvi(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE, GL.GL_MODULATE);
-		return(amb);
-	}
+    protected Color setenv(GL gl) {
+	gl.glTexEnvi(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE, GL.GL_MODULATE);
+	return(amb);
+    }
 	
-	public void init(GL gl) {
-		if(lbtex == null)
-			lbtex = new TexI(ljusboll);
-		gl.glClearColor(255, 255, 255, 255);
-	}
+    public void init(GL gl) {
+	if(lbtex == null)
+	    lbtex = new TexI(ljusboll);
+	gl.glClearColor(255, 255, 255, 255);
+    }
 	
-	public void subrend(GOut g) {
-		GL gl = g.gl;
-		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-		if(ll != null) {
-			for(Lumin lum : ll) {
-				Coord sc = lum.gob.sc.add(lum.off).add(-lum.sz, -lum.sz);
-				g.image(lbtex, sc, new Coord(lum.sz * 2, lum.sz * 2));
-			}
-		}
+    public void subrend(GOut g) {
+	GL gl = g.gl;
+	gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+	if(ll != null) {
+	    for(Lumin lum : ll) {
+		Coord sc = lum.gob.sc.add(lum.off).add(-lum.sz, -lum.sz);
+		g.image(lbtex, sc, new Coord(lum.sz * 2, lum.sz * 2));
+	    }
 	}
+    }
 	
-	public void update(Collection<Lumin> objs) {
-		ll = objs;
-		update();
-		ll = null;
-	}
+    public void update(Collection<Lumin> objs) {
+	ll = objs;
+	update();
+	ll = null;
+    }
 }
--- src/haven/TexRT.java
+++ src/haven/TexRT.java
@@ -4,91 +4,91 @@ import javax.media.opengl.*;
 import javax.media.opengl.glu.GLU;
 
 public abstract class TexRT extends TexGL {
-	private GLPbuffer pbuf;
-	private boolean inited = false;
-	public Profile prof = new Profile(300);
-	private Profile.Frame curf;
+    private GLPbuffer pbuf;
+    private boolean inited = false;
+    public Profile prof = new Profile(300);
+    private Profile.Frame curf;
 	
-	public TexRT(Coord sz) {
-		super(sz);
-		pbuf = null;
-	}
+    public TexRT(Coord sz) {
+	super(sz);
+	pbuf = null;
+    }
 	
-	public void dispose() {
-		if(pbuf != null)
-			pbuf.destroy();
-		inited = false;
-		super.dispose();
-	}
+    public void dispose() {
+	if(pbuf != null)
+	    pbuf.destroy();
+	inited = false;
+	super.dispose();
+    }
 	
-	protected abstract void init(GL gl);
-	protected abstract void subrend(GOut g);
+    protected abstract void init(GL gl);
+    protected abstract void subrend(GOut g);
 	
-	protected void fill(GOut g) {
-		if(pbuf == null) {
-			GLDrawableFactory df = GLDrawableFactory.getFactory();
-			if(!df.canCreateGLPbuffer())
-				throw(new RuntimeException("No pbuffer support"));
-			GLCapabilities caps = new GLCapabilities();
-			caps.setDoubleBuffered(false);
-			caps.setAlphaBits(8);
-			caps.setRedBits(8);
-			caps.setGreenBits(8);
-			caps.setBlueBits(8);
-			pbuf = df.createGLPbuffer(caps, null, tdim.x, tdim.y, g.ctx);
-			pbuf.addGLEventListener(new GLEventListener() {
-					public void display(GLAutoDrawable d) {
-						GL gl = d.getGL();
-						subrend2(gl);
-					}
+    protected void fill(GOut g) {
+	if(pbuf == null) {
+	    GLDrawableFactory df = GLDrawableFactory.getFactory();
+	    if(!df.canCreateGLPbuffer())
+		throw(new RuntimeException("No pbuffer support"));
+	    GLCapabilities caps = new GLCapabilities();
+	    caps.setDoubleBuffered(false);
+	    caps.setAlphaBits(8);
+	    caps.setRedBits(8);
+	    caps.setGreenBits(8);
+	    caps.setBlueBits(8);
+	    pbuf = df.createGLPbuffer(caps, null, tdim.x, tdim.y, g.ctx);
+	    pbuf.addGLEventListener(new GLEventListener() {
+		    public void display(GLAutoDrawable d) {
+			GL gl = d.getGL();
+			subrend2(gl);
+		    }
 			
-					public void init(GLAutoDrawable d) {
-						GL gl = d.getGL();
-						gl.glClearColor(0, 0, 0, 1);
-						gl.glColor3f(1, 1, 1);
-						gl.glPointSize(4);
-						gl.glEnable(GL.GL_BLEND);
-						//gl.glEnable(GL.GL_LINE_SMOOTH);
-						gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-						TexRT.this.init(gl);
-						reshape(d, 0, 0, tdim.x, tdim.y);
-					}
+		    public void init(GLAutoDrawable d) {
+			GL gl = d.getGL();
+			gl.glClearColor(0, 0, 0, 1);
+			gl.glColor3f(1, 1, 1);
+			gl.glPointSize(4);
+			gl.glEnable(GL.GL_BLEND);
+			//gl.glEnable(GL.GL_LINE_SMOOTH);
+			gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+			TexRT.this.init(gl);
+			reshape(d, 0, 0, tdim.x, tdim.y);
+		    }
 
-					public void reshape(GLAutoDrawable d, int x, int y, int w, int h) {
-						GL gl = d.getGL();
-						GLU glu = new GLU();
-						gl.glMatrixMode(GL.GL_PROJECTION);
-						gl.glLoadIdentity();
-						glu.gluOrtho2D(0, w, 0, h);
-					}
+		    public void reshape(GLAutoDrawable d, int x, int y, int w, int h) {
+			GL gl = d.getGL();
+			GLU glu = new GLU();
+			gl.glMatrixMode(GL.GL_PROJECTION);
+			gl.glLoadIdentity();
+			glu.gluOrtho2D(0, w, 0, h);
+		    }
 			
-					public void displayChanged(GLAutoDrawable d, boolean cp1, boolean cp2) {}
-				});
-		}
-		update();
+		    public void displayChanged(GLAutoDrawable d, boolean cp1, boolean cp2) {}
+		});
 	}
+	update();
+    }
 	
-	public void update() {
-		curf = prof.new Frame();
-		if(pbuf != null)
-			pbuf.display();
-		curf.tick("gl-out");
-		curf.fin();
-	}
+    public void update() {
+	curf = prof.new Frame();
+	if(pbuf != null)
+	    pbuf.display();
+	curf.tick("gl-out");
+	curf.fin();
+    }
 	
-	private void subrend2(GL gl) {
-		curf.tick("gl-in");
-		GOut g = new GOut(gl, pbuf.getContext(), dim);
-		subrend(g);
-		curf.tick("render");
-		if(id < 0)
-			throw(new RuntimeException("Negative tex id when updating pbuf texture"));
-		gl.glBindTexture(GL.GL_TEXTURE_2D, id);
-		if(!inited) {
-			gl.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, tdim.x, tdim.y, 0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, null);
-			inited = true;
-		}
-		gl.glCopyTexSubImage2D(GL.GL_TEXTURE_2D, 0, 0, 0, 0, 0, dim.x, dim.y);
-		curf.tick("copy");
+    private void subrend2(GL gl) {
+	curf.tick("gl-in");
+	GOut g = new GOut(gl, pbuf.getContext(), dim);
+	subrend(g);
+	curf.tick("render");
+	if(id < 0)
+	    throw(new RuntimeException("Negative tex id when updating pbuf texture"));
+	gl.glBindTexture(GL.GL_TEXTURE_2D, id);
+	if(!inited) {
+	    gl.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, tdim.x, tdim.y, 0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, null);
+	    inited = true;
 	}
+	gl.glCopyTexSubImage2D(GL.GL_TEXTURE_2D, 0, 0, 0, 0, 0, dim.x, dim.y);
+	curf.tick("copy");
+    }
 }
