7bf97f69b327b9f6676aea6b99348b18f6f24340
Fredrik Tolf <fredrik@dolda2000.com>
2009-02-24 20:00:09 +0100

    Changed Logwindow to reasonable whitespace usage.

--- src/haven/Logwindow.java
+++ src/haven/Logwindow.java
@@ -1,26 +1,26 @@
 package haven;
 
 public class Logwindow extends HWindow {
-	Textlog log;
+    Textlog log;
 	
-	static {
-		Widget.addtype("slenlog", new WidgetFactory() {
-			public Widget create(Coord c, Widget parent, Object[] args) {
-				return(new Logwindow(parent, (String)args[0]));
-			}
-		});
-	}
-	
-	public Logwindow(Widget parent, String title) {
-		super(parent, title);
-		log = new Textlog(Coord.z, sz, this);
-	}
-	
-	public void uimsg(String name, Object... args) {
-		if(name == "log") {
-			log.append((String)args[0]);
-		} else {
-			super.uimsg(name, args);
+    static {
+	Widget.addtype("slenlog", new WidgetFactory() {
+		public Widget create(Coord c, Widget parent, Object[] args) {
+		    return(new Logwindow(parent, (String)args[0]));
 		}
+	    });
+    }
+	
+    public Logwindow(Widget parent, String title) {
+	super(parent, title);
+	log = new Textlog(Coord.z, sz, this);
+    }
+	
+    public void uimsg(String name, Object... args) {
+	if(name == "log") {
+	    log.append((String)args[0]);
+	} else {
+	    super.uimsg(name, args);
 	}
+    }
 }
