9087b3272983e24f4a3bf4200acad5200ee1fef7
Fredrik Tolf <fredrik@dolda2000.com>
2009-04-18 01:58:37 +0200

    Use reasonable whitespace for CheckBox.java.

--- src/haven/CheckBox.java
+++ src/haven/CheckBox.java
@@ -1,39 +1,39 @@
 package haven;
 
 public class CheckBox extends Widget {
-	static Tex box, mark;
-	boolean a = false;
-	Text lbl;
+    static Tex box, mark;
+    boolean a = false;
+    Text lbl;
 	
-	static {
-		Widget.addtype("chk", new WidgetFactory() {
-			public Widget create(Coord c, Widget parent, Object[] args) {
-				return(new CheckBox(c, parent, (String)args[0]));
-			}
-		});
-		box = Resource.loadtex("gfx/hud/chkbox");
-		mark = Resource.loadtex("gfx/hud/chkmark");
-	}
+    static {
+	Widget.addtype("chk", new WidgetFactory() {
+		public Widget create(Coord c, Widget parent, Object[] args) {
+		    return(new CheckBox(c, parent, (String)args[0]));
+		}
+	    });
+	box = Resource.loadtex("gfx/hud/chkbox");
+	mark = Resource.loadtex("gfx/hud/chkmark");
+    }
 	
-	public CheckBox(Coord c, Widget parent, String lbl) {
-		super(c, box.sz(), parent);
-		this.lbl = Text.std.render(lbl, java.awt.Color.WHITE);
-		sz = box.sz().add(this.lbl.sz());
-	}
+    public CheckBox(Coord c, Widget parent, String lbl) {
+	super(c, box.sz(), parent);
+	this.lbl = Text.std.render(lbl, java.awt.Color.WHITE);
+	sz = box.sz().add(this.lbl.sz());
+    }
 	
-	public boolean mousedown(Coord c, int button) {
-		if(button != 1)
-			return(false);
-		a = !a;
-		wdgmsg("ch", a);
-		return(true);
-	}
+    public boolean mousedown(Coord c, int button) {
+	if(button != 1)
+	    return(false);
+	a = !a;
+	wdgmsg("ch", a);
+	return(true);
+    }
 
-	public void draw(GOut g) {
-		g.image(lbl.tex(), new Coord(box.sz().x, box.sz().y - lbl.sz().y));
-		g.image(box, Coord.z);
-		if(a)
-			g.image(mark, Coord.z);
-		super.draw(g);
-	}
+    public void draw(GOut g) {
+	g.image(lbl.tex(), new Coord(box.sz().x, box.sz().y - lbl.sz().y));
+	g.image(box, Coord.z);
+	if(a)
+	    g.image(mark, Coord.z);
+	super.draw(g);
+    }
 }
