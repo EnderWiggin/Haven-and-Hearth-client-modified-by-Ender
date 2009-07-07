f089aab11f292e57f4659ea53dde9ea5d54f4b4d
Fredrik Tolf <fredrik@dolda2000.com>
2009-07-07 17:56:28 +0200

    Fixed default resource URL.

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -26,7 +26,7 @@ public class Config {
 	    authuser = getprop("haven.authuser", null);
 	    authserv = getprop("haven.authserv", null);
 	    defserv = getprop("haven.defserv", null);
-	    if(!(p = getprop("haven.resurl", "http://www.havenandhearth.com/res/")).equals(""))
+	    if(!(p = getprop("haven.resurl", "https://www.havenandhearth.com/res/")).equals(""))
 		resurl = new URL(p);
 	    fullscreen = getprop("haven.fullscreen", "off").equals("on");
 	    loadwaited = getprop("haven.loadwaited", null);
