3b7a7a126e128a9273de0dc80af183a09c5b1b68
Fredrik Tolf <fredrik@dolda2000.com>
2009-07-11 20:37:02 +0200

    Added keyboard shortcuts for flower menus.

--- src/haven/FlowerMenu.java
+++ src/haven/FlowerMenu.java
@@ -193,4 +193,17 @@ public class FlowerMenu extends Widget {
 	    anim.tick();
 	super.draw(g);
     }
+    
+    public boolean type(char key, java.awt.event.KeyEvent ev) {
+	if((key >= '0') && (key <= '9')) {
+	    int opt = (key == '0')?10:(key - '1');
+	    if(opt < opts.length)
+		wdgmsg("cl", opt);
+	    return(true);
+	} else if(key == 27) {
+	    wdgmsg("cl", -1);
+	    return(true);
+	}
+	return(false);
+    }
 }
