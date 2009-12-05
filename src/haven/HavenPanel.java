bce44a929a7625c9e46a0d38778661eaabc54e11
Fredrik Tolf <fredrik@dolda2000.com>
2009-12-05 22:27:17 +0100

    Removed the erroneous use of a ThreadLocal in the global profile.

--- src/haven/HavenPanel.java
+++ src/haven/HavenPanel.java
@@ -47,7 +47,7 @@ public class HavenPanel extends GLCanvas implements Runnable {
     private Resource lastcursor = null;
     public Coord mousepos = new Coord(0, 0);
     public Profile prof = new Profile(300);
-    private ThreadLocal<Profile.Frame> curf = new ThreadLocal<Profile.Frame>();
+    private Profile.Frame curf = null;
     private SyncFSM fsm = null;
     private static final GLCapabilities caps;
     static {
@@ -241,21 +241,21 @@ public class HavenPanel extends GLCanvas implements Runnable {
 	gl.glLoadIdentity();
 	gl.glOrtho(0, getWidth(), 0, getHeight(), -1, 1);
 	TexRT.renderall(g);
-	if(curf.get() != null)
-	    curf.get().tick("texrt");
+	if(curf != null)
+	    curf.tick("texrt");
 
 	gl.glMatrixMode(GL.GL_PROJECTION);
 	gl.glLoadIdentity();
 	gl.glOrtho(0, getWidth(), getHeight(), 0, -1, 1);
 	gl.glClearColor(0, 0, 0, 1);
 	gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-	if(curf.get() != null)
-	    curf.get().tick("cls");
+	if(curf != null)
+	    curf.tick("cls");
 	synchronized(ui) {
 	    ui.draw(g);
 	}
-	if(curf.get() != null)
-	    curf.get().tick("draw");
+	if(curf != null)
+	    curf.tick("draw");
 
 	if(Config.dbtext) {
 	    g.atext("FPS: " + fps, new Coord(10, 545), 0, 1);
@@ -370,17 +370,17 @@ public class HavenPanel extends GLCanvas implements Runnable {
 	    while(true) {
 		then = System.currentTimeMillis();
 		if(Config.profile)
-		    curf.set(prof.new Frame());
+		    curf = prof.new Frame();
 		synchronized(ui) {
 		    if(ui.sess != null)
 			ui.sess.glob.oc.ctick();
 		    dispatch();
 		}
-		if(curf.get() != null)
-		    curf.get().tick("dsp");
+		if(curf != null)
+		    curf.tick("dsp");
 		uglyjoglhack();
-		if(curf.get() != null)
-		    curf.get().tick("aux");
+		if(curf != null)
+		    curf.tick("aux");
 		frames++;
 		now = System.currentTimeMillis();
 		if(now - then < fd) {
@@ -388,8 +388,8 @@ public class HavenPanel extends GLCanvas implements Runnable {
 			events.wait(fd - (now - then));
 		    }
 		}
-		if(curf.get() != null)
-		    curf.get().tick("wait");
+		if(curf != null)
+		    curf.tick("wait");
 		if(now - fthen > 1000) {
 		    fps = frames;
 		    frames = 0;
@@ -398,10 +398,8 @@ public class HavenPanel extends GLCanvas implements Runnable {
 		    texhit = texmiss = 0;
 		    fthen = now;
 		}
-		if(curf.get() != null) {
-		    curf.get().fin();
-		    curf.remove();
-		}
+		if(curf != null)
+		    curf.fin();
 		if(Thread.interrupted())
 		    throw(new InterruptedException());
 	    }
