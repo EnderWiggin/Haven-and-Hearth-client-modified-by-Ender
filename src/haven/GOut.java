f89ad0dfef9a7fe4f74f356b481b3c14fbe661e8
Fredrik Tolf <fredrik@dolda2000.com>
2010-04-21 22:12:44 +0200

    Fire-proofed GOut.chcolor. :P

--- src/haven/GOut.java
+++ src/haven/GOut.java
@@ -227,6 +227,14 @@ public class GOut {
     }
     
     public void chcolor(int r, int g, int b, int a) {
+	if(r < 0)   r = 0;
+	if(r > 255) r = 255;
+	if(g < 0)   g = 0;
+	if(g > 255) g = 255;
+	if(b < 0)   b = 0;
+	if(b > 255) b = 255;
+	if(a < 0)   a = 0;
+	if(a > 255) a = 255;
 	chcolor(new Color(r, g, b, a));
     }
 	
