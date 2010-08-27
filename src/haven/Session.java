92cd7b9f739882d80474074ef8f8516804b8d36b
Fredrik Tolf <fredrik@dolda2000.com>
2010-08-27 13:02:54 +0200

    Wake up sworker only when all objdata is processed.

--- src/haven/Session.java
+++ src/haven/Session.java
@@ -321,9 +321,9 @@ public class Session {
 			objacks.put(id, new ObjAck(id, frame, System.currentTimeMillis()));
 		    }
 		}
-		synchronized(sworker) {
-		    sworker.notifyAll();
-		}
+	    }
+	    synchronized(sworker) {
+		sworker.notifyAll();
 	    }
 	}
 		
