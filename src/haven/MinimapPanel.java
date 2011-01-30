4166207f607b221901816e6e9a4c7f2e254a1d3c
Ender Wiggin <Andrii.Gook@gmail.com>
2011-01-30 19:42:33 +0200

    added buttons to display claims and villages zones of control to MinimapPanel

--- src/haven/MinimapPanel.java
+++ src/haven/MinimapPanel.java
@@ -12,15 +12,64 @@ public class MinimapPanel extends Window {
     MiniMap mm;
     public MinimapPanel(Coord c, Coord sz, Widget parent) {
 	super(c, sz, parent, "Minimap");
+	mrgn = Coord.z;
 	fbtn.visible = true;
-	mm = new MiniMap(Coord.z, new Coord(125, 125), this, ui.mainview);
+	cbtn.visible = false;
+	{
+	    new IButton(new Coord(0, -2), this, Resource.loadimg("gfx/hud/slen/dispauth"), Resource.loadimg("gfx/hud/slen/dispauthd")) {
+		private boolean v = false;
+		
+		public void click() {
+		    MapView mv = ui.root.findchild(MapView.class);
+		    BufferedImage tmp = down;
+		    down = up;
+		    up = tmp;
+		    hover = tmp;
+		    if(v) {
+			mv.disol(2, 3);
+			v = false;
+		    } else {
+			mv.enol(2, 3);
+			v = true;
+		    }
+		}
+	    };
+	}
+	{
+	    new IButton(new Coord(0, 4), this, Resource.loadimg("gfx/hud/slen/dispclaim"), Resource.loadimg("gfx/hud/slen/dispclaimd")) {
+		private boolean v = false;
+		
+		public void click() {
+		    MapView mv = ui.root.findchild(MapView.class);
+		    BufferedImage tmp = down;
+		    down = up;
+		    up = tmp;
+		    hover = tmp;
+		    if(v) {
+			mv.disol(0, 1);
+			v = false;
+		    } else {
+			mv.enol(0, 1);
+			v = true;
+		    }
+		}
+	    };
+	}
+	
+	mm = new MiniMap(new Coord(0, 32), new Coord(125, 125), this, ui.mainview);
 	pack();
 	this.c = new Coord( MainFrame.getInnerSize().x - this.sz.x, 7);
     }
     
+    protected void placecbtn() {
+	fbtn.c = new Coord(wsz.x - 3 - Utils.imgsz(cbtni[0]).x, 3).add(mrgn.inv().add(wbox.tloff().inv()));
+	//fbtn.c = new Coord(cbtn.c.x - 1 - Utils.imgsz(fbtni[0]).x, cbtn.c.y);
+    }
+    
     public void draw(GOut g) {
 	super.draw(g);
-	g.image(grip, sz.sub(gzsz));
+	if(!folded)
+	    g.image(grip, sz.sub(gzsz));
     }
     
     public boolean mousedown(Coord c, int button) {
