78a9087c4b0cc6936851c1c556512ca35c55b00e
Ender Wiggin <Andrii.Gook@gmail.com>
2011-02-04 17:34:54 +0200

    added python file that can generate 'action' non-image layers

---
+++ resources/HnH_actions.py
@@ -0,0 +1,14 @@
+from struct import pack
+
+def generate(parent, v, name, req, hk, *params):
+    tmp = str(parent) + "\x00"
+    tmp += pack("H", v)
+    tmp += str(name) + "\x00"
+    tmp += str(req) + "\x00"
+    tmp += str(hk) + "\x00"
+    tmp += pack("H",len(params))
+    for param in params:
+        tmp += str(param) + "\x00"
+    return "action\x00" +pack("L",len(tmp))+tmp
+
+    
