3846aac2e9cf81e346bdcbd48c7204e0f584a48f
EnderWiggin <andrii.gook@gmail.com>
2011-03-06 21:28:54 +0200

    ported to python 3

--- resources/HnH_actions.py
+++ resources/HnH_actions.py
@@ -1,12 +1,12 @@
 from struct import pack
 
 def generate(parent, v, name, req, hk, *params):
-    tmp = str(parent) + "\x00"
+    tmp = parent + b"\x00"
     tmp += pack("H", v)
-    tmp += str(name) + "\x00"
-    tmp += str(req) + "\x00"
-    tmp += str(hk) + "\x00"
+    tmp += name + b"\x00"
+    tmp += req + b"\x00"
+    tmp += hk + b"\x00"
     tmp += pack("H",len(params))
     for param in params:
-        tmp += str(param) + "\x00"
-    return "action\x00" +pack("L",len(tmp))+tmp
+        tmp += param + b"\x00"
+    return b"action\x00" +pack("L",len(tmp))+tmp
