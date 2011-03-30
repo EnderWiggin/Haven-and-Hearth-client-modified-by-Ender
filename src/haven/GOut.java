074b8ba0d676cd875c18cd4a21e183b7789a66fc
EnderWiggin <andrii.gook@gmail.com>
2011-03-30 23:41:53 +0300

    Once more fixed less than 5° ellipse drawing.

--- src/haven/GOut.java
+++ src/haven/GOut.java
@@ -205,14 +205,12 @@ public class GOut {
 	texsel(-1);
 	gl.glBegin(GL.GL_TRIANGLE_FAN);
 	vertex(c);
-	for(int i = a1; i <= a2; i += 5) {
+	for(int i = a1; i < a2; i += 5) {
 	    double a = (i * Math.PI * 2) / 360.0;
 	    vertex(c.add((int)(Math.cos(a) * r.x), -(int)(Math.sin(a) * r.y)));
 	}
-	if((a1 - a2)%5 > 0) {
-	    double a = (a2 * Math.PI * 2) / 360.0;
-	    vertex(c.add((int)(Math.cos(a) * r.x), -(int)(Math.sin(a) * r.y)));
-	}
+	double a = (a2 * Math.PI * 2) / 360.0;
+	vertex(c.add((int)(Math.cos(a) * r.x), -(int)(Math.sin(a) * r.y)));
 	gl.glEnd();
 	checkerr();
     }
