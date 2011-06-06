ffe7f32bdb6c9a2adb4365e6c92e59f988b7bb57
EnderWiggin <Andrii.Gook@gmail.com>
2011-06-06 12:09:19 +0300

    updated changelog and bumped version

--- src/haven/MainFrame.java
+++ src/haven/MainFrame.java
@@ -121,7 +121,7 @@ public class MainFrame extends Frame implements Runnable, FSMan {
     }
 
     public MainFrame(int w, int h) {
-	super("Haven and Hearth (modified by Ender v05.06.11)");
+	super("Haven and Hearth (modified by Ender v06.06.11)");
 	innerSize = new Dimension(w, h);
 	centerPoint = new Point(innerSize.width / 2, innerSize.height / 2);
 	screenSZ = new Coord(Toolkit.getDefaultToolkit().getScreenSize());
