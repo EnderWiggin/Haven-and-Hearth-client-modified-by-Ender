8f99a70a9d019c974d05371161bc7ecd22ad42ad
EnderWiggin <andrii.gook@gmail.com>
2011-07-13 22:51:14 +0300

    updated changelog and bumped version

--- src/haven/MainFrame.java
+++ src/haven/MainFrame.java
@@ -33,7 +33,7 @@ import java.util.*;
 
 @SuppressWarnings("serial")
 public class MainFrame extends Frame implements Runnable, FSMan {
-    private static final String TITLE = "Haven and Hearth (modified by Ender v11.07.11)";
+    private static final String TITLE = "Haven and Hearth (modified by Ender v13.07.11)";
     HavenPanel p;
     ThreadGroup g;
     DisplayMode fsmode = null, prefs = null;
